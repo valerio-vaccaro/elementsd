7c89a4108023d83929b3d4932d29bfa400fd199d
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2022-12-01 10:03:10 +0100

    add elements-22.0.2 support

--- src/versions.rs
+++ src/versions.rs
@@ -1,7 +1,10 @@
 pub const HAS_FEATURE: bool = cfg!(any(feature = "0_21_0", feature = "0_18_1_12",));
 
-#[cfg(not(any(feature = "0_21_0", feature = "0_18_1_12",)))]
+#[cfg(not(any(feature = "22_0_2", feature = "0_21_0", feature = "0_18_1_12",)))]
 pub const VERSION: &str = "N/A";
+
+#[cfg(feature = "22_0_2")]
+pub const VERSION: &str = "22.0.2";
 
 #[cfg(feature = "0_21_0")]
 pub const VERSION: &str = "elements-0.21.0";
